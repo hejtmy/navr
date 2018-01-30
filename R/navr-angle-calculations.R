89e9528a812a1b2fb735ee3429de6a60c84df994
hejtmy <hejtmy@gmail.com>
2018-01-29 18:37:13 -0800

    Adds angle and distance functions

---
+++ R/navr-angle-calculations.R
@@ -0,0 +1,71 @@
+# converts positive and negative angles to 0-360
+# asumes it is not below -360
+# 390 is converted to 30, -40 to 320 etc
+#' Title
+#'
+#' @param angle
+#'
+#' @return
+#' @export
+#'
+#' @examples
+angle_to_360 <- function(angle){
+  return((angle + 360) %% 360)
+}
+
+#' Converts angle to -180 to 180 difference
+#'
+#' @description
+#'
+#'
+#' @param angle
+#'
+#' @return number between -180 to 180
+#' @export
+#'
+#' @examples
+#' > angle_to_180(90)
+#' [1] 90
+#' > angle_to_180(270)
+#' [1] -90
+angle_to_180 <- function(angle){
+  angle <- ((angle + 180) %% 360) - 180
+  return(angle)
+}
+
+
+#' Converts Pi radian angle to degrees
+#'
+#' @param radian
+#'
+#' @return
+#' @export
+#'
+#' @examples
+radian_to_angle <- function(radian){
+  angle <- radian/pi * 180
+  if(angle < 0) angle <- 360 + angle
+  return(angle)
+}
+
+#' Calculates angle
+#'
+#' @param pos_from
+#' @param pos_to
+#' @param zero_vec defines which axis should correspond to 0 degrees. defaults to c(0,1) (Y axis)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+angle_from_positions <- function(pos_from, pos_to, zero_vec = c(0,1)){
+  if(length(pos_from) != 2 || length(pos_to) != 2){
+    error("ERROR:angle_from_positions, DESCRIPTION:input does not have two 2d position")
+  }
+  target_vector <- pos_to - pos_from
+  #' ATAN takes Y and X, but we want to scale it against Z axis,
+  #' therefore Y in carthesian, so the input is reversed
+  theta <- atan2(target_vector[1], target_vector[2])
+  angle <- radian_to_angle(theta)
+  return(angle)
+}
---
+++ R/navr-distance-calculations.R
@@ -0,0 +1,38 @@
+#' Euclidian distance between two points
+#' @description
+#' Calculates euclidian distance between two points.
+#'
+#' @param point1 2D vector of X and Y position
+#' @param point2 2D vector of X and Y position
+#'
+#' @return numeric or NA if not valid
+#' @export
+#'
+#' @examples
+#' > print(euclid_distance(c(0,0), c(1,1)))
+#' [1] 1.414214
+euclid_distance <- function(point1, point2){
+  # VALIDATE
+  return(sqrt(sum((point2 - point1)^2)))
+}
+
+#' Eucilidan distance covered between each row
+#'
+#' @description
+#' Calculates distance between points in each row
+#'
+#' @param points m x 2 matrix with first column being X and second designating Y coordinates
+#'
+#' @return vector of distances of length m (prepends 0 to the beginning)
+#' @export
+#'
+#' @examples
+#'
+euclid_distance_between_rows <- function(points){
+  points_shifted <- rbind(c(0,0), points[1:nrow(points) - 1, ])
+  points_sub <- points - points_shifted
+  vec_sums <- apply(points_sub, 1, function(x) sqrt(sum(x ^ 2)))
+  return(vec_sums)
+}
+
+
