00951d027f57b048f8520eb513e836fbe22b2654
hejtmy <hejtmy@gmail.com>
2018-01-29 22:46:32 -0800

    Adds multiplot

--- R/navr-complex-plots.R
+++ R/navr-complex-plots.R
@@ -16,3 +16,41 @@ create_plot <- function(xlim = NA, ylim = NA){
   if (!is.na(ylim)) plt <- plt + ylim(ylim)
   return(plt)
 }
+
+
+#' Title
+#'
+#' @param plotlist
+#' @param cols
+#' @param layout
+#'
+#' @return
+#' @export
+#'
+#' @examples
+multiplot <- function(plotlist = NULL, cols = 1, layout = NULL) {
+  library(grid)
+  numPlots <- length(plotlist)
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  if (numPlots == 1) {
+    print(plotlist[[1]])
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      print(plotlist[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                         layout.pos.col = matchidx$col))
+    }
+  }
+}
