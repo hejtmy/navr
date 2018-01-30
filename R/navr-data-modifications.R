b800478e340687450e0dcbbaa7118dc29da22d27
hejtmy <hejtmy@gmail.com>
2018-01-30 02:31:36 -0800

    Fixes typo bug

--- R/navr-data-modifications.R
+++ R/navr-data-modifications.R
@@ -9,7 +9,7 @@
 #' add_angle_difference(player_log, player_log$Rotation.x, "x")
 #'
 add_angle_difference <- function(df_log, rotation, name){
-  new_col_name <- paste0("angle_diff_", axis)
+  new_col_name <- paste0("angle_diff_", name)
 
   angle_diffs <- c(0, diff(rotation))
   angle_diffs <- angle_to_180(axis_angle_diffs)
