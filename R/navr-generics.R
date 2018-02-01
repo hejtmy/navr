ce6db1552a68ca3a1a979d529d5939139d24ee13
hejtmy <hejtmy@gmail.com>
2018-02-01 00:20:35 -0800

    Adds goal positions

--- R/navr-generics.R
+++ R/navr-generics.R
@@ -1,9 +1,25 @@
 #' Generic function
 #'
-#' @param trialId
+#' @param obj
+#' @param trialId vector of trial ids
+#' @param ... Optional parameters
 #'
 #' @return ggplot object
 #' @export
 plot_trial_path <- function (obj, trialId, ...){
   UseMethod("plot_trial_path")
+}
+
+#' Adds goal positions to the object
+#'
+#' @param obj
+#' @param df data frame with goal positions with appropriate column naming
+#' @param ...
+#'
+#' @return modified object
+#' @export
+#'
+#' @examples
+add_goal_positions <- function(obj, df, ...){
+  UseMethod("add_goal_positions")
 }
