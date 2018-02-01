87692791a4ab3a180aa413c3ace078fdbda3237d
hejtmy <hejtmy@gmail.com>
2018-02-01 00:51:41 -0800

    Adds more generic methods

--- R/navr-generics.R
+++ R/navr-generics.R
@@ -14,7 +14,7 @@ plot_trial_path <- function (obj, trialId, ...){
 #'
 #' @param obj
 #' @param df data frame with goal positions with appropriate column naming
-#' @param ...
+#' @param ... aditional arguments
 #'
 #' @return modified object
 #' @export
@@ -23,3 +23,76 @@ plot_trial_path <- function (obj, trialId, ...){
 add_goal_positions <- function(obj, df, ...){
   UseMethod("add_goal_positions")
 }
+
+#' Title
+#'
+#' @param obj
+#' @param trialId
+#' @param ... aditional arguments
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_trial_log <- function(obj, trialId, ...){
+  UseMethod("get_trial_log")
+}
+
+#' Returns list with trial times
+#'
+#' @param obj
+#' @param trialId
+#' @param ...
+#'
+#' @return list with start and end field
+#' @export
+#'
+#' @examples
+get_trial_times <- function(obj, trialId, ...){
+  UseMethod("get_trial_times")
+}
+
+#' Returns times when certain action was performend during a particular trial
+#'
+#' @param obj
+#' @param trialId
+#' @param action string with the action name
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_trial_action_times <- function(obj, trialId, action, ...){
+  UseMethod("get_trial_action_times")
+}
+
+#' Title
+#'
+#' @param obj
+#' @param trialId
+#' @param ...
+#'
+#' @return vector 2 of x and Y of the goal position
+#' @export
+#'
+#' @examples
+get_trial_goal_position <- function(obj, trialId, ...){
+  UseMethod("get_trial_goal_position")
+}
+
+
+#' Returns X and Y of a starting position
+#'
+#' @param obj
+#' @param trialId
+#' @param ...
+#'
+#' @return vector 2 of x and Y of the start position
+#' @export
+#'
+#' @examples
+get_trial_start_position <- function(obj, trialId, ...){
+  UseMethod("get_trial_start_position")
+}
+
