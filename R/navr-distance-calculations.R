cb6aa74b623f3f493e4e5b877066a0a596d1c8d0
hejtmy <hejtmy@gmail.com>
2018-06-07 19:52:02 -0700

    Fixes between row sums

--- R/navr-distance-calculations.R
+++ R/navr-distance-calculations.R
@@ -21,7 +21,7 @@ euclid_distance <- function(point1, point2){
 #' @description
 #' Calculates distance between points in each row
 #'
-#' @param points m x 2 matrix with first column being X and second designating Y coordinates
+#' @param points m x 2 matrix or data.frame with first column X and second Y coordinates
 #'
 #' @return vector of distances of length m (prepends 0 to the beginning)
 #' @export
@@ -29,7 +29,8 @@ euclid_distance <- function(point1, point2){
 #' @examples
 #'
 euclid_distance_between_rows <- function(points){
-  points_shifted <- rbind(c(0,0), points[1:nrow(points) - 1, ])
+  #first distance shoudl be rezo, that's why we copy the first row in the shifted matrix
+  points_shifted <- rbind(points[1,], points[1:nrow(points) - 1, ])
   points_sub <- points - points_shifted
   vec_sums <- apply(points_sub, 1, function(x) sqrt(sum(x ^ 2)))
   return(vec_sums)
