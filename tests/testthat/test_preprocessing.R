edaee6a2e3290e367d199b4f2bb40e380c750af5
hejtmy <hejtmy@gmail.com>
2019-02-17 15:05:52 +0100

    Adds distance tests

--- tests/testthat/test_preprocessing.R
+++ tests/testthat/test_preprocessing.R
@@ -3,4 +3,6 @@ context('Preprocessing data')
 test_that("Testing distance calculations", {
   obj <- navr_object
   obj_dist <- add_distances(obj)
+  expect_equal(round(sum(obj_dist$data$distance)), 14865) #needs stupid rounding
+  expect_equal(sum(obj_dist$data$distance), obj_dist$data$distance_total[nrow(obj_dist$data)])
 })
