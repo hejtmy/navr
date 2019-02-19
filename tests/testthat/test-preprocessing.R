0c3f0a8767990c60acad26615a332c5161e81f4c
hejtmy <hejtmy@gmail.com>
2019-02-19 19:42:59 +0100

    adds preprocessing and preparing things

--- tests/testthat/test-preprocessing.R
+++ tests/testthat/test-preprocessing.R
@@ -4,7 +4,8 @@ test_that("Testing distance calculations", {
   obj <- navr_object
   obj_dist <- add_distances(obj)
   expect_equal(round(sum(obj_dist$data$distance)), 14865) #needs stupid rounding
-  expect_equal(sum(obj_dist$data$distance), obj_dist$data$distance_total[nrow(obj_dist$data)])
+  expect_equal(sum(obj_dist$data$distance),
+               obj_dist$data$distance_total[nrow(obj_dist$data)])
 })
 
 test_that("Testing speed calculations", {
