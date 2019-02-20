37867dee27c4143e1730562e36df2a67ad4bf732
Lukáš Hejtmánek <hejtmy@gmail.com>
2019-02-20 13:25:59 +0100

    Fixes navr initialisation

--- R/navr-object.R
+++ R/navr-object.R
@@ -6,7 +6,7 @@
 #' @examples
 NavrObject <- function(){
   obj <- list()
-  ojb$data <- NULL
+  obj$data <- NULL
   obj$area_boundaries <- NULL
   class(obj) <- append(class(obj), "navr")
   return(obj)
