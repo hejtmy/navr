5b23115b8c273b263e8d4dff17613770031506b7
hejtmy <hejtmy@gmail.com>
2019-02-21 21:05:08 +0100

    fixes limits

--- R/navr-plot-elements.R
+++ R/navr-plot-elements.R
@@ -115,7 +115,7 @@ plot_add_direction <- function(plt, position, angle, len = 1, ...){
 #' @examples
 plot_add_limits <- function(plt, obj){
   if(is.null(obj$area_boundaries)) return(plt)
-  if(!is.null(obj$area_boundaries$x)) plt <- plt + ylim(obj$area_boundaries$x)
+  if(!is.null(obj$area_boundaries$x)) plt <- plt + xlim(obj$area_boundaries$x)
   if(!is.null(obj$area_boundaries$y)) plt <- plt + ylim(obj$area_boundaries$y)
   return(plt)
 }
